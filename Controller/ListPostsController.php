f82a3512dc833cebe8733764939d9bbaefbfbf98
Dzov <amelie.haladjian@gmail.com>
2018-02-03 18:46:49 +0100

    feat(theme): filter out unnecessary theme files, add twig template

--- Controller/ListPostsController.php
+++ Controller/ListPostsController.php
@@ -2,9 +2,13 @@
 
 namespace Blog\Controller;
 
+use Exception;
 use PostManager;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
 
 require 'Model/PostManager.php';
+require 'vendor/twig/twig/lib/Twig/Loader/Filesystem.php';
 
 /**
  * @author Amélie-Dzovinar Haladjian
@@ -13,13 +17,40 @@ class ListPostsController
 {
     private $em;
 
+    public function listPostsAction()
+    {
+        $posts = $this->getPosts();
+
+        $this->renderTemplate('index.php', [$posts]);
+    }
+
     public function __construct()
     {
         $this->em = new PostManager();
     }
 
-    public function getPosts()
+    private function getPosts()
     {
         return $this->em->findAllPosts();
+    }
+
+    private function renderTemplate(string $path, array $parameters)
+    {
+        $loader = new Twig_Loader_Filesystem('Views/templates');
+        $twig = new Twig_Environment(
+            $loader, array(
+                'cache' => 'Blog/vendor/twig/twig/src/Util',
+            )
+        );
+
+        try {
+            $twig->load($path);
+
+            echo $twig->render($path, $parameters);
+
+        } catch (Exception $e) {
+            var_dump($e->getMessage());
+        }
+
     }
 }
--- index.php
+++ index.php
@@ -4,14 +4,8 @@ namespace Blog;
 
 use Blog\Controller\ListPostsController;
 
-
-
-
 require 'Controller/ListPostsController.php';
 
 $controller = new ListPostsController();
 
-$posts = $controller->getPosts();
-
-require 'Views/bona/index.html';
-
+$controller->listPostsAction();
