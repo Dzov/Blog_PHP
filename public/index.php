807224e6d87d9e8c248fb5c0b482e9e8046d3105
Dzov <amelie.haladjian@gmail.com>
2018-04-09 13:32:08 +0200

    refacto(posts): remove commented lines

--- public/index.php
+++ public/index.php
@@ -16,23 +16,4 @@ $url = str_replace($_SERVER['BASE'] . '/', '', $_SERVER['REQUEST_URI']);
 $router->get($url);
 
 
-/*
-    switch ($_GET['url']) {
-        case 'listPosts':
-            PostController::listPostsAction();
-            break;
-        case 'post':
-            if (isset($_GET['p'])) {
-                PostController::showPostAction((int) $_GET['p']);
-            }
-            break;
-        case 'comment':
-            CommentController::addCommentAction($_GET['p'], htmlspecialchars($_POST['author']), htmlspecialchars($_POST['content']));
-            break;
-        case 'contact':
-            break;
-        default :
-            HomeController::listRecentPostsAction();
-}*/
-
 
